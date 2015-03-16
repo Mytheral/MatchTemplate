56a5c0c8a833c6686dc30afe78824d3f549011bf
Mytheral <mytheral@gmail.com>
2015-03-16 13:53:58 -0400

    Push to Repo

--- OpenCV-TemplateMatching/src/com/example/opencv_templatematching/TemplateMatching.java
+++ OpenCV-TemplateMatching/src/com/example/opencv_templatematching/TemplateMatching.java
@@ -11,11 +11,13 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import android.graphics.Bitmap;
+import android.util.Log;
 
 public class TemplateMatching 
 {
-	Bitmap returnInit = null;
-	Bitmap returnFinal = null;
+	final String TAG = "TemplateMatching";
+	private Bitmap returnInit = null;
+	private Bitmap returnFinal = null;
 	
 	public Bitmap getReturnInit() 
 	{
@@ -44,20 +46,28 @@ public class TemplateMatching
         findDistances[2] = distanceBetweenPoints(topThree[1], topThree[2]);
 
         int longest = 0;
-        if (findDistances[0] > findDistances[1] && findDistances[0] > findDistances[2]) {
+        if (findDistances[0] > findDistances[1] && findDistances[0] > findDistances[2]) 
+        {
             longest = 0;
-        } else if (findDistances[1] > findDistances[0] && findDistances[1] > findDistances[2]) {
+        } else if (findDistances[1] > findDistances[0] && findDistances[1] > findDistances[2]) 
+        {
             longest = 1;
-        } else if (findDistances[2] > findDistances[0] && findDistances[2] > findDistances[1]) {
+        } else if (findDistances[2] > findDistances[0] && findDistances[2] > findDistances[1]) 
+        {
             longest = 2;
         }
 
         int shortest = 0;
-        if (findDistances[0] < findDistances[1] && findDistances[0] < findDistances[2]) {
+        if (findDistances[0] < findDistances[1] && findDistances[0] < findDistances[2]) 
+        {
             shortest = 0;
-        } else if (findDistances[1] < findDistances[0] && findDistances[1] < findDistances[2]) {
+        } 
+        else if (findDistances[1] < findDistances[0] && findDistances[1] < findDistances[2]) 
+        {
             shortest = 1;
-        } else if (findDistances[2] < findDistances[0] && findDistances[2] < findDistances[1]) {
+        } 
+        else if (findDistances[2] < findDistances[0] && findDistances[2] < findDistances[1]) 
+        {
             shortest = 2;
         }
 
@@ -163,11 +173,12 @@ public class TemplateMatching
     	//FIXME: Resize is currently Fubar, update when OpenCV4Android is updated
     	Size currentSize = new Size(initImg.width(), initImg.height());
     	Imgproc.warpAffine(initImg, initImg, rotMatrix, currentSize, Imgproc.INTER_LINEAR + Imgproc.CV_WARP_FILL_OUTLIERS);
+    	Imgproc.warpAffine(finalImg, finalImg, rotMatrix, currentSize, Imgproc.INTER_LINEAR + Imgproc.CV_WARP_FILL_OUTLIERS);
     	
     	Utils.matToBitmap(initImg, returnInit);
     	Utils.matToBitmap(finalImg, returnFinal);
     	returnInit = Bitmap.createScaledBitmap(returnInit, newWidth, newHeight, true);
-    	//returnFinal = Bitmap.createScaledBitmap(returnFinal, newWidth, newHeight, true);
+    	returnFinal = Bitmap.createScaledBitmap(returnFinal, newWidth, newHeight, true);
     	
     	int cropWidth = 519;
     	int cropHeight = 413;
@@ -186,10 +197,20 @@ public class TemplateMatching
     	int endX = startX + fixedWidth;
     	int endY = startY + fixedHeight;
     	
+    	if (endX > newWidth || endX < 0)
+    	{
+    		Log.v(TAG, "rotate(Mat, Mat, double, Point): Trying to Crop Image Outside of Bounds. Image Width: " + newWidth + " Attempting to Crop: " + endX);
+    	}
+    	
+    	if (endY > newHeight || endY < 0)
+    	{
+    		Log.v(TAG, "rotate(Mat, Mat, double, Point): Trying to Crop Image Outside of Bounds. Image Height: " + newHeight + " Attempting to Crop: " + endY);
+    	}
+    	
     	//FIXME: Add error catching
     	
     	returnInit = Bitmap.createBitmap(returnInit, startX, startY, fixedWidth, fixedHeight);
-    	//returnFinal = Bitmap.createBitmap(returnFinal, startX, startY, fixedWidth, fixedHeight);
+    	returnFinal = Bitmap.createBitmap(returnFinal, startX, startY, fixedWidth, fixedHeight);
     }
     
 	public TemplateMatching(Bitmap initFile, Bitmap finalFile, Bitmap templateFile, int match_method) 
@@ -202,12 +223,14 @@ public class TemplateMatching
         Mat orginalImg = new Mat();
         
         returnInit = initFile;
+        returnFinal = finalFile;
         
         //img = Highgui.imread(inFile.toString());
         //templ = Highgui.imread(templateFile.toString());
 
         Utils.bitmapToMat(initFile, initImg);
         Utils.bitmapToMat(templateFile, templateImg);
+        Utils.bitmapToMat(finalFile, finalImg);
         
         initImg.copyTo(orginalImg);
         
@@ -215,22 +238,25 @@ public class TemplateMatching
         
         for (int i=0; i<3; i++)
         {
-	        // / Create the result matrix
+	        //Create the result matrix
 	        int result_cols = initImg.cols() - templateImg.cols() + 1;
 	        int result_rows = initImg.rows() - templateImg.rows() + 1;
 	        Mat result = new Mat(result_rows, result_cols, CvType.CV_32FC1);
 	
-	        // / Do the Matching and Normalize
+	        //Do the Matching and Normalize
 	        Imgproc.matchTemplate(initImg, templateImg, result, match_method);
 	        Core.normalize(result, result, 0, 1, Core.NORM_MINMAX, -1, new Mat());
 	
-	        // / Localizing the best match with minMaxLoc
+	        //Localizing the best match with minMaxLoc
 	        MinMaxLocResult mmr = Core.minMaxLoc(result);
 	
 	        Point matchLoc;
-	        if (match_method == Imgproc.TM_SQDIFF || match_method == Imgproc.TM_SQDIFF_NORMED) {
+	        if (match_method == Imgproc.TM_SQDIFF || match_method == Imgproc.TM_SQDIFF_NORMED) 
+	        {
 	            matchLoc = mmr.minLoc;
-	        } else {
+	        } 
+	        else 
+	        {
 	            matchLoc = mmr.maxLoc;
 	        }
 	
@@ -238,8 +264,12 @@ public class TemplateMatching
 	        pointArray[i].x = matchLoc.x;
 	        pointArray[i].y = matchLoc.y;
 	        
-	        // / Show me what you got
-	        Core.rectangle(initImg, matchLoc, new Point(matchLoc.x + templateImg.cols(), matchLoc.y + templateImg.rows()), new Scalar(0, 255, 0), -1);
+	        //Show me what you got
+	        Core.rectangle(initImg, 
+	        			   matchLoc, 
+	        			   new Point(matchLoc.x + templateImg.cols(), matchLoc.y + templateImg.rows()), 
+	        			   new Scalar(0, 255, 0), 
+	        			   -1);
         }
         
         int[] orientation = determineOrientation(pointArray);
@@ -248,10 +278,10 @@ public class TemplateMatching
         double yDiff = pointArray[orientation[1]].y - pointArray[orientation[0]].y;
         double angle = Math.toDegrees(Math.atan2(yDiff, xDiff));
         
-//        if (angle < 0)
-//        {
-//        	angle += 180;
-//        }
+        if (angle < 0)
+        {
+        	angle += 180;
+        }
 
         rotate(initImg, finalImg, angle, pointArray[orientation[1]]);
 	}	
